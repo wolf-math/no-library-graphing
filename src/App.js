f508c46a86627e5f512bb5672bd4d031408fe242
aaron <aaron.wolf@gmail.com>
2021-06-29 14:23:38 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const burgersByMonth = {
+  january: 5,
+  february: 13,
+  march: 4,
+  april: 5,
+  may: 10,
+  june: 14,
+  july: 5,
+  august: 7,
+  september: 8,
+  october: 7,
+  november: 6,
+  december: 2
+};
+
+
+let arr = Object.values(burgersByMonth);
+let max = Math.max(...arr);
+
+const BarChartContainer = styled.div`
+  width: 100%;
+  height: 67vh;
+`;
+
+const YValues = styled.div`
+  height: 100%;
+`;
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>🍔🍔🍔 Hamburgers Eaten per Month 🍔🍔🍔</h1>
+      <BarChartContainer>
+        <YValues>
+
+        </YValues>
+        {Object.entries(burgersByMonth).map(item => console.log(item))}
+      </BarChartContainer>
     </div>
   );
 }
